dec591ef07693c5ef5a7b3e385f1e2333748474b
rniman <shinmg00@gmail.com>
2025-06-20 01:46:10 +0900

    [Add] Create Trigger box component and print on overlap event
    - Implemented Trigger class using UBoxComponent
    - Bound overlap event to log when actor enters trigger

---
+++ Source/CryptRaider/TriggerComponent.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TriggerComponent.h"
+
+UTriggerComponent::UTriggerComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+// Called when the game starts
+void UTriggerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+    //UE_LOG(LogTemp, Display, TEXT("Trigger Component!"));
+}
+
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    //UE_LOG(LogTemp, Display, TEXT("Trigger Component Is Ticking"));
+
+	TArray<AActor*> Actors;
+	GetOverlappingActors(Actors);
+
+	// int32 index = 0;
+	// while(index < Actors.Num())
+	// {
+	// 	FString ActorName = Actors[index]->GetActorNameOrLabel();
+	// 	UE_LOG(LogTemp, Display, TEXT("Overlapping: %s"), *ActorName); 
+	// 	++index;
+	// }
+	// for (int32 i = 0; i < Actors.Num(); ++i)
+	// {
+	// 	FString ActorName = Actors[i]->GetActorNameOrLabel();
+	// 	UE_LOG(LogTemp, Display, TEXT("Overlapping: %s"), *ActorName); 
+	// }
+
+	for(AActor* Actor: Actors)
+	{
+		FString ActorName = Actor->GetActorNameOrLabel();
+		UE_LOG(LogTemp, Display, TEXT("Overlapping: %s"), *ActorName); 
+	}
+}
